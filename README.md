e3bb00aeb7a46d4402a198a66c112553125fc5cf
irene macapinlac <irenekaymac@gmail.com>
2021-07-08 22:50:58 +1000

    added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Quiz Game on the Python Command Line
+
+---
+
+First project to learn and get familiar with Python syntax.
+
+
+###### Languages used:
+* Python
+
+###### What I learnt from this project:
+* How to set up Python
+* Using if statements using Python syntax
--- quiz-game.py
+++ quiz-game.py
@@ -1,11 +1,32 @@
-print("Let's Play!")
+print("Welcome to my Python Quiz Game")
 
 playing = input("Do you want to play? ")
 
 if playing != "yes":
     quit()
 
-print("Okay! Let's play: )")
+print("Okay! Let's play :D")
 
 answer = input("What does CPU stand for? ")
-if answer == "central processing unit"
+if answer == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer == "graphics processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer == "random access memory":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer == "power supply unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
