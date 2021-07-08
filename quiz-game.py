0868df8411de0b26f840c4cc6319e76937d0eaf2
irene macapinlac <irenekaymac@gmail.com>
2021-07-08 22:58:17 +1000

    .lower() functionality

--- quiz-game.py
+++ quiz-game.py
@@ -2,31 +2,32 @@ print("Welcome to my Python Quiz Game")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+# .lower() will convert text the user writes into lower case
+if playing.lower() != "yes":
     quit()
 
 print("Okay! Let's play :D")
 
 answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("Correct!")
 else:
     print("Incorrect!")
 
 answer = input("What does GPU stand for? ")
-if answer == "graphics processing unit":
+if answer.lower() == "graphics processing unit":
     print("Correct!")
 else:
     print("Incorrect!")
 
 answer = input("What does RAM stand for? ")
-if answer == "random access memory":
+if answer.lower() == "random access memory":
     print("Correct!")
 else:
     print("Incorrect!")
 
 answer = input("What does PSU stand for? ")
-if answer == "power supply unit":
+if answer.lower() == "power supply unit":
     print("Correct!")
 else:
     print("Incorrect!")
